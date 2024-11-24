3127011b0b5819d316de694c97105872d0e4af9d
Daniel Hassett <danielhassett@Daniels-Mini.attlocal.net>
2024-11-24 15:20:29 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import ContactList from './components/ContactList';
+import SelectedContact from "./components/SelectedContact";
+import './App.css'
+
+export default function App() {
+  const [selectedContactId, setSelectedContactId] = useState(null);
+  return (
+    <>
+    {selectedContactId ? (
+      <div>Selected Contact View</div>
+    ) : (
+      <ContactList />
+    )}
+    </>
+  );
+}
+
+
